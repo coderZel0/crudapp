6a51a9d2bc369400b5de20e0c774db056fd739bf
coderZel0 <obitouhiha0026@gamil.com>
2022-09-09 00:05:52 +0530

    added first components

---
+++ src/components/ListContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import User from './User'
+
+const ListContainer = () => {
+  return (
+    <div className='list_container'>
+        <div className='container_head'>
+        
+        </div>
+    </div>
+  )
+}
+
+export default ListContainer
---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div>Userlist</div>
+  )
+}
+
+export default User
